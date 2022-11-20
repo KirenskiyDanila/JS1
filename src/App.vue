5e7ca0a164f8b2c98762ff3cafa250f0ccb6d5a7
Киренский Данила <80262480+KirenskiyDanila@users.noreply.github.com>
2022-11-20 18:09:45 +0300

    123

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <body class="bg">
+  <div class="bg">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  </body>
 </template>
 
 <script>
@@ -14,13 +17,18 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="css">
+body, html {
+  height: 100%;
+}
+
+.bg {
+  background-image: url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full page/img(20).jpg");
+
+  height: 70em;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import App from "@/App";
 
 createApp(App).mount('#app')
